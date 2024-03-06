bc79e3013a46d995a33e5503fd8b7217762bf396
Murilo Reigado <murilo.sreigado@gmail.com>
2024-03-06 10:43:11 -0300

    First commit

---
+++ Quotz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quotz
+//
+//  Created by Murilo da Silva Reigado on 06/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
